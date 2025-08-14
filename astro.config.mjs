626e0e1cac4b5d1556e44e68152b21c950b10aaf
AD Gomez <garyeld12@gmail.com>
2025-08-14 14:53:45 -0400

    updating site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+// Si usas Tailwind vía el plugin oficial, también:
+// import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
-})
+  site: 'https://adglobalpay.github.io', // tu user orginal
+  base: '/adglobal/',                    // nombre exacto del repo (Project Pages)
+  trailingSlash: 'ignore',
+  integrations: [
+    react(),
+    // tailwind()
+  ],
+});
